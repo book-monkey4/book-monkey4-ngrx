90e8b173a04e60a2479298d6aad47b5145d78292
The Buildbot <buildbot@angular2buch.de>
2020-07-23 12:43:24 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/book-monkey4-ngrx-src/commit/f6c52cd453467b0ff48834629fda01667cdcd9cc
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-ngrx-src/43

--- src/app/books/store/book.effects.marbles.spec.ts
+++ src/app/books/store/book.effects.marbles.spec.ts
@@ -13,7 +13,6 @@ import { book } from './my-test-helper';
 describe('BookEffects', () => {
   let actions$: Actions;
   let effects: BookEffects;
-  let bs: jasmine.SpyObj<BookStoreService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,19 +22,19 @@ describe('BookEffects', () => {
         {
           provide: BookStoreService,
           useValue: {
-            getAll: jasmine.createSpy()
+            getAll: () => of([])
           }
         }
       ]
     });
 
     effects = TestBed.inject(BookEffects);
-    bs = TestBed.inject(BookStoreService) as any;
   });
 
   it('should fire loadBooksSuccess for loadBooks', () => {
     const books = [book(1), book(2), book(3)];
-    bs.getAll.and.callFake(() => of(books));
+    const bs = TestBed.inject(BookStoreService);
+    spyOn(bs, 'getAll').and.callFake(() => of(books));
 
     actions$ =       hot('--a-', { a: loadBooks() });
     const expected = cold('--b', { b: loadBooksSuccess({ data: books }) });
