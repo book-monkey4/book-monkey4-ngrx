7ebdfc9016c7f04514b972c4738ce2c3462885f7
The Buildbot <buildbot@angular2buch.de>
2020-07-23 12:06:11 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/book-monkey4-ngrx-src/commit/d8b71e2cf5edec10001c688d072d43ac232d82d8
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-ngrx-src/40

--- src/app/books/store/book.effects.spec.ts
+++ src/app/books/store/book.effects.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { Actions } from '@ngrx/effects';
 import { provideMockActions } from '@ngrx/effects/testing';
-
-import { BookEffects } from './book.effects';
-import { of } from 'rxjs';
-import { loadBooks, loadBooksSuccess } from './book.actions';
-import { BookStoreService } from 'src/app/shared/book-store.service';
-import { Book } from 'src/app/shared/book';
 import { Action } from '@ngrx/store';
+import { of } from 'rxjs';
+import { BookStoreService } from 'src/app/shared/book-store.service';
+
+import { loadBooks, loadBooksSuccess } from './book.actions';
+import { BookEffects } from './book.effects';
+import { book } from './my-test-helper';
+
 
 describe('BookEffects', () => {
   let actions$: Actions;
@@ -30,20 +31,17 @@ describe('BookEffects', () => {
     effects = TestBed.inject<BookEffects>(BookEffects);
   });
 
-  it('should be created', async () => {
-    expect(effects).toBeTruthy();
-  });
-
   it('should load all books via BookStoreService', () => {
     actions$ = of(loadBooks());
 
-    const testBooks = [{ isbn: 'X' }] as Book[];
+    const testBooks = [book(1), book(2), book(3)];
     const bs = TestBed.inject(BookStoreService);
     spyOn(bs, 'getAll').and.callFake(() => of(testBooks));
 
     let dispatchedAction: Action;
     effects.loadBooks$.subscribe(action => dispatchedAction = action);
 
+    expect(bs.getAll).toHaveBeenCalled();
     expect(dispatchedAction).toEqual(loadBooksSuccess({ data: testBooks }));
   });
 });
