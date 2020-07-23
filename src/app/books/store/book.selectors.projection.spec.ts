c936a0936256828c6421e6546d706790f7b2c11f
The Buildbot <buildbot@angular2buch.de>
2020-07-23 10:06:34 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/book-monkey4-ngrx-src/commit/1614a764e6f015dc1f1cf9dac0fc4afdbc1ec555
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey4-ngrx-src/39

---
+++ src/app/books/store/book.selectors.projection.spec.ts
@@ -0,0 +1,16 @@
+import { selectAllBooks } from './book.selectors';
+import { book } from './my-test-helper';
+
+describe('Book Selectors', () => {
+
+  it('should select all books', () => {
+    const books = [book(1), book(2), book(3)];
+    const bookState = {
+      books,
+      loading: false
+    };
+
+    const result = selectAllBooks.projector(bookState); 
+    expect(result).toEqual(books);
+  });
+});
